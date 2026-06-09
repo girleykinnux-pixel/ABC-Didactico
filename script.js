8f3363486782a6382fa5eafb8239af61db42bad4
girley <girleykinnux@gmail.com>
2026-06-09 14:44:43 -0500

    se agregaron las imagenes en script

---
+++ script.js
@@ -0,0 +1,38 @@
+// ==========================================
+// Base de datos del Abecedario (Según imágenes)
+// ==========================================
+const datosAbecedario = [
+  { letra: "A", palabra: "Abeja", imagen: "Abeja.jpg", tipo: "vocal" },
+  { letra: "B", palabra: "Búho", imagen: "Buho.jpeg", tipo: "consonante" },
+  { letra: "C", palabra: "Casa", imagen: "Casa.jpg", tipo: "consonante" },
+  { letra: "D", palabra: "Delfín", imagen: "Delfin.jpg", tipo: "consonante" },
+  { letra: "E", palabra: "Elefante", imagen: "Elefante.jpeg", tipo: "vocal" },
+  { letra: "F", palabra: "Foca", imagen: "Foca.jpg", tipo: "consonante" },
+  { letra: "G", palabra: "Gato", imagen: "Gato.jpg", tipo: "consonante" },
+  { letra: "H", palabra: "Helado", imagen: "Helado.jpg", tipo: "consonante" },
+  { letra: "I", palabra: "Iglesia", imagen: "Iglesia.jpg", tipo: "vocal" },
+  { letra: "J", palabra: "Jirafa", imagen: "Jirafa.jpg", tipo: "consonante" },
+  { letra: "K", palabra: "Koala", imagen: "Koala.jpeg", tipo: "consonante" },
+  { letra: "L", palabra: "León", imagen: "Leon.jpg", tipo: "consonante" },
+  { letra: "M", palabra: "Manzana", imagen: "Manzana.jpg", tipo: "consonante" },
+  { letra: "N", palabra: "Nubes", imagen: "Nubes.png", tipo: "consonante" },
+  { letra: "Ñ", palabra: "Ñoño", imagen: "Ñoño.jpg", tipo: "consonante" },
+  { letra: "O", palabra: "Oso", imagen: "Oso.jpg", tipo: "vocal" },
+  { letra: "P", palabra: "Pelota", imagen: "Pelota.jpg", tipo: "consonante" },
+  { letra: "Q", palabra: "Queso", imagen: "Queso.png", tipo: "consonante" },
+  { letra: "R", palabra: "Ratón", imagen: "Raton.png", tipo: "consonante" },
+  { letra: "S", palabra: "Sol", imagen: "Sol.jpg", tipo: "consonante" },
+  { letra: "T", palabra: "Tortuga", imagen: "Tortuga.jpg", tipo: "consonante" },
+  { letra: "U", palabra: "Uva", imagen: "Uva.jpg", tipo: "vocal" },
+  { letra: "V", palabra: "Vaca", imagen: "Vaca.jpg", tipo: "consonante" },
+  { letra: "W", palabra: "Wafle", imagen: "wafle.jpg", tipo: "consonante" }, // Nota: la w está en minúscula en tu archivo
+  {
+    letra: "X",
+    palabra: "Xilófono",
+    imagen: "Xilofono.png",
+    tipo: "consonante",
+  },
+  { letra: "Y", palabra: "Yoyo", imagen: "Yoyo.jpg", tipo: "consonante" },
+  { letra: "Z", palabra: "Zapato", imagen: "Zapato.jpg", tipo: "consonante" },
+];
+
